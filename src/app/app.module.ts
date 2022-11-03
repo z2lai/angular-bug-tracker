ba69a3165cc65a189b794476fb2543d56036079b
Zheng Lai <zhenglai3@gmail.com>
2022-11-02 21:42:10 -0400

    Added hero interface and two-way binding to heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -12,6 +13,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
