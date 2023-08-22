648e238249d8799573b7a63c4d70333709d8c28a
Zheng Lai <zhenglai3@gmail.com>
2023-08-21 21:01:37 -0400

    Implemented display of heroes mock data with interactivity to select a hero, display their details and edit their name

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'bt-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'WindStorm'
+export class HeroesComponent implements OnInit {  
+  heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
+  hero = { id: 20, name: 'Tornado' };
+  
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
   }
 
   constructor() { }
