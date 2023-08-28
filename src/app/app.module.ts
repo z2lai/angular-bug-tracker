ed7e25c85733c79c12e5fb3542d6c0370cf45671
Zheng Lai <zhenglai3@gmail.com>
2023-08-27 21:14:45 -0400

    Upgraded to Angular 15 and removed unused package, ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -23,7 +22,6 @@ import { HeroPageComponent } from './hero-page/hero-page.component';
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule,
     AppRoutingModule
   ],
   providers: [],
